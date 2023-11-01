376ee5e372985f984c54e07b520febef9870ce6b
anushae6 <141826601+anushae6@users.noreply.github.com>
2023-11-01 22:43:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 
 import setuptools
 
-VERSION = [1, 3, 1]
+VERSION = [1, 3, 2]
 
 
 def get_long_desc():
@@ -43,7 +43,7 @@ setuptools.setup(
         'grpcio',
         'grpcio-tools',
         'sqlalchemy',
-        'protobuf=3.20.2',
+        'protobuf==3.20.2',
     ],
     classifiers=[
         "Operating System :: POSIX :: Linux",
